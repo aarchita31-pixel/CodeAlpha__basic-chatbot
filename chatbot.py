90627f9f3a7a1ebd0dc816001927485abf60b50a
kumbar Archita <aarchita31@gmail.com>
2025-09-21 20:13:01 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+# Simple Rule-Based Chatbot
+def basic_chatbot():
+    print("🤖 Chatbot: Hello! Type 'bye' to end the chat.")
+    while True:
+        user_input=input("You:").lower() # take input and convert to lowercase
+        if user_input=="hello":
+            print("Chatbot: Hii")
+        elif  user_input=="how are you":
+            print("Chatbot: I'm fine,thanks!")
+        elif user_input=="bye":
+            print("Chatbot: Goodbye!")
+            break # Exit the loop and end the chat
+        else:
+            print("i don't understand that.")
+basic_chatbot()
